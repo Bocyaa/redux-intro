32a158259720314e6e34d22a7ddd9a034fabb946
Abdubositkhon Khabirov <xabirovbob@gmail.com>
2025-02-22 22:57:17 +0100

    Implement store.js, reducer and action functions

--- src/store.js
+++ src/store.js
@@ -21,7 +21,12 @@ function reducer(state = initialState, action) {
     case 'account/requestLoan':
       if (state.loan > 0) return state;
       // Finish later ...
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case 'account/payLoan':
       return {
         ...state,
@@ -36,4 +41,24 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: 'account/deposit', payload: 500 });
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return { type: 'account/requestLoan', payload: { amount, purpose } };
+}
+
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(500));
+store.dispatch(requestLoan(1200, 'Need money'));
+store.dispatch(payLoan());
+console.log(store.getState());
